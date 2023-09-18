05a5588d6c77d79f6e1ece0dc1001e7e2c971015
BurritoBandit28 <yansalmon@gmail.com>
2023-09-18 17:38:09 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod accounts;
+
+use std::string::ToString;
+use accounts::check_password_strength;
+use accounts::PasswordFeedback;
+
+
+fn main() {
+    println!("Hello, world!");
+    let mut pass: String = String::new();
+    std::io::stdin().read_line(&mut pass).expect("oh no!");
+    let result : PasswordFeedback = check_password_strength(pass.trim().to_string());
+    println!("{} {}", result.message, result.code);
+}
